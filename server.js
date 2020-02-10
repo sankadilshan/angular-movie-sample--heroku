fb51d44d7bad8298cad7b8ba74aaeb962a2985d8
sanka9686 <sankadilshan9686@gmail.com>
2020-02-10 12:08:22 +0530

    better

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/angularmovies'));
+app.use(express.static(__dirname + '/dist'));
+// default Heroku PORT
+app.listen(process.env.PORT || 8080);
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/angularmovies/index.html'));
 });
 
-// default Heroku PORT
-app.listen(process.env.PORT || 3000);
+console.log('cpnsole listening');
